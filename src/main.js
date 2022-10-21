987134ef141781208496da3cc82f1c545668e16a
guilhermehub12 <guilhermee1312@hotmail.com>
2022-10-21 12:43:21 -0300

    new cc masks

--- src/main.js
+++ src/main.js
@@ -59,6 +59,41 @@ const cardNumberPattern = {
       cardtype: "mastercard",
     },
     {
+      mask: '0000 000000 00000',
+      regex: /^3[47]\d{0,13}/,
+      cardtype: 'american express'
+    },
+    {
+      mask: '0000 0000 0000 0000',
+      regex: /^(?:6011|65\d{0,2}|64[4-9]\d?)\d{0,12}/,
+      cardtype: 'discover'
+    },
+    {
+      mask: '0000 000000 0000',
+      regex: /^3(?:0([0-5]|9)|[689]\d?)\d{0,11}/,
+      cardtype: 'diners'
+    },
+    {
+      mask: '0000 000000 00000',
+      regex: /^(?:2131|1800)\d{0,11}/,
+      cardtype: 'jcb15'
+    },
+    {
+      mask: '0000 0000 0000 0000',
+      regex: /^(?:35\d{0,2})\d{0,12}/,
+      cardtype: 'jcb'
+    },
+    {
+      mask: '0000 0000 0000 0000',
+      regex: /^(?:5[0678]\d{0,2}|6304|67\d{0,2})\d{0,12}/,
+      cardtype: 'maestro'
+    },
+    {
+      mask: '0000 0000 0000 0000',
+      regex: /^62\d{0,14}/,
+      cardtype: 'unionpay'
+    },
+    {
       mask: "0000 0000 0000 0000",
       cardtype: "default",
     },
